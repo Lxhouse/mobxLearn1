a11f1eda8f2b2d931bee8c85e2096324869d0a1d
zhemu <lingx@ibuscloud.com>
2023-02-07 15:28:55 +0800

    feat: 准备改造

---
+++ src/components/trackPlayback/PalySlider/index.jsx
@@ -0,0 +1,14 @@
+import { Slider } from "antd";
+
+/**播放条 */
+const PlaySlider = () => {
+  return (
+    <Slider
+      className="pro"
+      //   value={playbackProgress}
+      max={200}
+      //   onChange={progressSlider}
+    />
+  );
+};
+export default PlaySlider;
